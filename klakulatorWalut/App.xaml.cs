e862cf8e77d04fe5cadff92ca66ff4e560812619
Paweł <paweladamowicz2890@gamil.com>
2023-10-10 12:59:05 +0200

    Add project files.

---
+++ klakulatorWalut/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace klakulatorWalut
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ klakulatorWalut/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace klakulatorWalut
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void przeliczBtnClicked(object sender, EventArgs e)
+        {
+            int kwota = int.Parse(kwotaEnt.Text);
+            double wynik = 0;
+            string waluta = "";
+            bool czyEu = rbtnEu.IsChecked;
+            if (czyEu)
+            {
+                wynik = kwota * 4.55;
+                waluta = "PLN";
+            }
+            else
+            {
+                wynik = kwota / 4.55;
+                waluta = "EUR";
+            }
+            wynikLbl.Text=wynik.ToString("0.00")+waluta;
+        }
+    }
+}
